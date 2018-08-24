aaf8181bc66f20dacea8ea73c5ff47896bba61b0
shangqingliiu <shangqingl@tujia.com>
2018-08-24 11:08:58 +0800

    'init'

---
+++ eInterview/app.js
@@ -0,0 +1,50 @@
+//app.js
+App({
+
+ globalData: {
+   avatarUrl: "",//用户头像
+   nickName: "11232232"//用户昵称
+ },
+  onLaunch: function() {
+    //调用API从本地缓存中获取数据
+    var logs = wx.getStorageSync('logs') || []
+    logs.unshift(Date.now())
+    wx.setStorageSync('logs', logs)
+   
+  },
+
+  getUserInfo: function(cb) {
+    var that = this
+    if (this.globalData.userInfo) {
+      typeof cb == "function" && cb(this.globalData.userInfo)
+    } else {
+      //调用登录接口
+      wx.getUserInfo({
+        withCredentials: false,
+        success: function(res) {
+          that.globalData.userInfo = res.userInfo
+          typeof cb == "function" && cb(that.globalData.userInfo)
+        }
+      })
+    }
+  },
+
+  globalData: {
+    userInfo: null
+  },
+
+   /**
+   * 页面的初始数据
+   */
+ 
+ /**
+  *点击添加地址事件
+  */
+ add_address_fun: function () {
+  wx.navigateTo({
+   url: 'add_address/add_address',
+  })
+ },
+
+
+})
